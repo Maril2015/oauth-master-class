326bdc24641545435db12fc862fa6655ce9401bb
Maril2015 <159425013+Maril2015@users.noreply.github.com>
2024-02-09 12:03:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 window.onload = () => {
   document.getElementById("button").onclick = () => {
-    window.YaAuthSuggest.init(
+ YaAuthSuggest.init(
       {
         client_id: "7774ada10177476e830fb57e417784bf",
         response_type: "token",
         redirect_uri: "https://oauth-master-class-three.vercel.app/token.html",
       },
       "https://oauth-master-class-three.vercel.app/",
-      {
+      )
        .then(({ handler }) => handler())
       .then(async (data) => {
         const result = await fetchYandexData(data.access_token);
